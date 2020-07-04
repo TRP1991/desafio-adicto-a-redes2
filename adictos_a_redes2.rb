c5d09e94152961cd3e751b0200aafc81d2eda4cc
Tamara Rubilar <ta.rubilar@gmail.com>
2020-07-03 23:34:57 -0400

    wrong test. Error in result variable

--- adictos_a_redes2.rb
+++ adictos_a_redes2.rb
@@ -19,3 +19,11 @@ def scan_addicts2(array)
             result.push "mal"
         elsif array [i] >=90
             result.push "mejorable"
+        else
+            result.push "bien"
+        end
+    end
+    result 
+end
+
+print scan_addicts2 ([120, 90, 600, 30, 90, 10, 200, 180, 500])
